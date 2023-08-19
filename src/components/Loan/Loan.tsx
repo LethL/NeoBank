63255fbaab627f4e034ad4d8a3b38a930a37e243
Dmitry Goncharov <carribean_08@mail.ru>
2023-08-19 11:28:06 +0300

    feat: add tooltip component

--- src/components/Loan/Loan.tsx
+++ src/components/Loan/Loan.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Loan.css';
 import LinkButton from '../LinkButton/LinkButton';
 import loanCard from '../../images/loan-card.png';
+import Tooltip from 'components/Tooltip/Tooltip';
 
 export default function Loan() {
   return (
@@ -13,18 +14,24 @@ export default function Loan() {
           commission and interest.
         </p>
         <div className="loan__wrapper">
-          <div>
-            <p className="loan__text loan__text_accent">Up to 160 days</p>
-            <p className="loan__text">No percent</p>
-          </div>
-          <div>
-            <p className="loan__text loan__text_accent">Up to 600 000 ₽</p>
-            <p className="loan__text">Credit limit</p>
-          </div>
-          <div>
-            <p className="loan__text loan__text_accent">0 ₽</p>
-            <p className="loan__text">Card service is free</p>
-          </div>
+          <Tooltip text="When repaying the full debt up to 160 days.">
+            <div>
+              <p className="loan__text loan__text_accent">Up to 160 days</p>
+              <p className="loan__text">No percent</p>
+            </div>
+          </Tooltip>
+          <Tooltip text="Over the limit willaccrue percent">
+            <div>
+              <p className="loan__text loan__text_accent">Up to 600 000 ₽</p>
+              <p className="loan__text">Credit limit</p>
+            </div>
+          </Tooltip>
+          <Tooltip text="Promotion valid until December 31, 20224.">
+            <div>
+              <p className="loan__text loan__text_accent">0 ₽</p>
+              <p className="loan__text">Card service is free</p>
+            </div>
+          </Tooltip>
         </div>
         <LinkButton text="Apply for card" link="#" />
       </div>
