031241b76265d99085ab9f932a9a562099698376
Dmitry Goncharov <carribean_08@mail.ru>
2023-08-28 14:56:07 +0300

    feat: add 404 page

--- src/constants/paths.tsx
+++ src/constants/paths.tsx
@@ -1,7 +1,9 @@
 export const HOME = '/';
 export const LOAN = 'loan';
+export const NOTFOUND = '*';
 
 export const PATH_LIST = {
   HOME,
   LOAN: `${HOME}${LOAN}`,
+  NOTFOUND,
 };
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { PATH_LIST } from '../constants/paths';
 import HomePage from '../pages/HomePage/HomePage';
 import LoanPage from '../pages/LoanPage/LoanPage';
+import NotFoundPage from 'components/NotFoundPage/NotFoundPage';
 
 export function Router() {
   return (
     <Routes>
       <Route path={PATH_LIST.HOME} element={<HomePage />} />
       <Route path={PATH_LIST.LOAN} element={<LoanPage />} />
+      <Route path={PATH_LIST.NOTFOUND} element={<NotFoundPage />} />
     </Routes>
   );
 }
